c0ddcde55d6a2c399429c6f0a661c84e63a738f3
Dasutein <dustine_2008@hotmail.com>
2018-08-27 18:52:36 +0800

    Rename Clear Spotify cache (UWP ver).bat to Clear Spotify cache (UWP).bat

---
+++ Clear Spotify cache (UWP).bat
@@ -0,0 +1,21 @@
+@ECHO OFF 
+
+ECHO This is a simple script to delete your Spotify cache (version 1.01).
+
+echo.
+
+PAUSE
+
+rd /S /Q C:\Users\%username%\AppData\Local\Packages\SpotifyAB.SpotifyMusic_zpdnekdrzrea0\LocalCache\Spotify\Data
+:: 
+
+echo.
+
+
+ECHO Spotify cache cleared!
+
+
+
+echo.
+
+PAUSE 
